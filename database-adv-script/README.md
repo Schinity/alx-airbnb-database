cca9a9d5c1375a045eb6049f25e8b9f759b01a97
chikwado stanley elo <chikwadostanley0@gmail.com>
2025-07-07 15:05:27 -0700

    Add SQL JOIN queries and README for advanced database task

---
+++ database-adv-script/README.md
@@ -0,0 +1,12 @@
+# SQL Joins Queries
+
+This script demonstrates complex SQL queries using various types of joins.
+
+## Queries Included
+
+- INNER JOIN: Bookings with Users
+- LEFT JOIN: Properties with Reviews (even if no reviews)
+- FULL OUTER JOIN: Users and Bookings (including unmatched records)
+
+## Author
+Stanley
---
+++ database-adv-script/joins_queries.sql
@@ -0,0 +1,25 @@
+SELECT
+  bookings.id AS booking_id,
+  bookings.start_date,
+  bookings.end_date,
+  users.id AS user_id,
+  users.first_name,
+  users.last_name
+FROM bookings
+INNER JOIN users ON bookings.user_id = users.id;
+SELECT
+  properties.id AS property_id,
+  properties.name,
+  reviews.id AS review_id,
+  reviews.rating,
+  reviews.comment
+FROM properties
+LEFT JOIN reviews ON properties.id = reviews.property_id;
+SELECT
+  users.id AS user_id,
+  users.first_name,
+  bookings.id AS booking_id,
+  bookings.start_date,
+  bookings.status
+FROM users
+FULL OUTER JOIN bookings ON users.id = bookings.user_id;
